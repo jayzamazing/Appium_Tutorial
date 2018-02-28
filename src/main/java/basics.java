3519db43bf628446fb807920a5377bfbf54057b4
jayzamazing <adrianjaylopez@gmail.com>
2018-02-28 17:28:11 -0500

    Added path to add wifi settings to app.

--- src/main/java/basics.java
+++ src/main/java/basics.java
@@ -1,4 +1,5 @@
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -6,5 +7,18 @@ import io.appium.java_client.android.AndroidElement;
 public class basics extends base {
 	public static void main(String[] args) throws MalformedURLException {
 		AndroidDriver<AndroidElement> driver = Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//find most items using xpath, id, className, and androidUIAutomator
+		/*
+		 * 	xpath syntax
+		 *	//tagName[@attribute='value'] //classname = tagname
+		 */
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		driver.findElementById("android:id/checkbox").click();
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+		driver.findElementByClassName("android.widget.EditText").sendKeys("blah");
+		driver.findElementsByClassName("android.widget.Button").get(1).click();
 	}
 }

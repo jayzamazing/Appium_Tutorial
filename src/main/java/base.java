6dc1283373fd436acda836107c70ee379703c554
jayzamazing <adrianjaylopez@gmail.com>
2018-02-28 14:56:53 -0500

    Added base.java and appium code to get app running in android.

---
+++ src/main/java/base.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+	public static void main(String[] args) throws MalformedURLException {
+		//get the apk file
+		File f = new File("src/main/java");
+		File fs = new File(f, "ApiDemos-debug.apk");
+		//create capabilities
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel XL API 27");
+		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+		//driver to connect to server
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+	}
+}

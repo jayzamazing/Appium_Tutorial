caad278ac1929d9e9716a626067461299eff4a7a
jayzamazing <adrianjaylopez@gmail.com>
2018-02-28 21:31:13 -0500

    Fix to finding inline element and added swipe method for the clock widget.

--- src/main/java/swipedemo.java
+++ src/main/java/swipedemo.java
@@ -1,4 +1,5 @@
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
@@ -11,10 +12,9 @@ public class swipedemo extends base{
 		//driver.findElementByAndroidUIAutomator("attribute(value)");
 		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
 		driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
-		driver.findElementByAndroidUIAutomator("test(\"2. Inline\")").click();
+		driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
 		driver.findElementByXPath("//*[@content-desc='9']").click();
-
-		
-		
+		TouchAction t = new TouchAction(driver);
+		t.press(driver.findElementByXPath("//*[@content-desc='15']")).waitAction(Duration.ofSeconds(2)).moveTo(driver.findElementByXPath("//*[@content-desc='45']")).perform();	
 	}
 }

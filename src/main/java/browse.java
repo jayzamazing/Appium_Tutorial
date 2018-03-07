ff3e2ba6fecc8f879b4cc384ccebeff3d819b1a4
jayzamazing <adrianjaylopez@gmail.com>
2018-03-07 14:20:43 -0500

    Changed url and performed some click events on website.

--- src/main/java/browse.java
+++ src/main/java/browse.java
@@ -6,9 +6,14 @@ import io.appium.java_client.android.AndroidElement;
 public class browse extends baseChrome {
 	public static void main(String[] args) throws MalformedURLException {
 		AndroidDriver<AndroidElement> driver = Capabilities("device");
-		driver.get("http://facebook.com");
-		driver.findElementByXPath("//*[@id=\"m_login_email\"]").sendKeys("qwerty");
-		driver.findElementByName("pass").sendKeys("12345");
-		driver.findElementByXPath("//button[@value='Log In']").click();
+//		driver.get("http://facebook.com");
+//		driver.findElementByXPath("//*[@id=\"m_login_email\"]").sendKeys("qwerty");
+//		driver.findElementByName("pass").sendKeys("12345");
+//		driver.findElementByXPath("//button[@value='Log In']").click();
+		
+		driver.get("http://cricbuzz.com");
+		driver.findElementByXPath("//a[@href='#menu']").click();
+		driver.findElementByCssSelector("a[title='Cricbuzz Home']").click();
+		System.out.println(driver.getCurrentUrl());
 	}
 }
